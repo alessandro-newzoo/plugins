91b2cf8db432dfe280af5cb7d3d8fbaf4abc8dc8
Tyler Jang <tyler@trunk.io>
2023-08-07 12:43:22 -0700

    normalize and bump timeouts

--- tests/utils/landing_state.ts
+++ tests/utils/landing_state.ts
@@ -71,6 +71,7 @@ const normalizeAutofix = ({ replacements: _replacements = [], ...rest }: Autofix
 const normalizeMessage = (message?: string) =>
   message
     ?.replace(fs.realpathSync(os.tmpdir()), "/tmp")
+    ?.replace(fs.realpathSync(os.tmpdir().replaceAll("\\", "/")), "/tmp")
     .replaceAll("\\", "/")
     .replace(/\/plugins_.{6}/gm, "/plugins_")
     .replace(".dup.", ".")
